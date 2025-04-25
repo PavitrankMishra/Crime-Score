5c273e11793c907e3c73ce8837aec88d2beb7f94
Pavitrank <pavitrank2002@gmail.com>
2025-04-26 00:15:44 +0530

    New Commit

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def index(): 
     return render_template("index.html") 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/predict', methods=['POST']) 
 def predict_result(): 
     # Mappings
